15c25204d3a62e0ef27806434c2a4aa984c74ba1
Tian He <tian.he@gojee.com>
2013-03-31 10:43:15 -0400

    first commit, bitly api query integrated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newswork::Application.config.secret_token = '3ce649c229b69ab85cc0b379ac283b7c5324d8abb5c15531edc418d926942c2fc32744194df7b9f6b237457ebc7813c775210d682b93d15d5ae951ab7f8c5e5b'
